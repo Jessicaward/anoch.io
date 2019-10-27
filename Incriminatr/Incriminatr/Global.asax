1a5a4e9c6e250b680e31a8f9de7e899bb06ac5ca
Fergus Coulter <fergus.coulteriom@gmail.com>
2019-10-27 10:47:19 +0000

    deleted files

--- Incriminatr/Incriminatr/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Incriminatr.MvcApplication" Language="C#" %>
--- Incriminatr/Incriminatr/Models/LobbyViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Incriminatr.Models
-{
-    public class LobbyViewModel
-    {
-        public List<ChatRoom> ChatRooms { get; set; }
-    }
-}
